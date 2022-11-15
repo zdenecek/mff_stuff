4150efa0447a00865f4d9434e221ba4b40a0fc64
zdenecek <zdnek.tomis@gmail.com>
2022-11-15 19:47:36 +0100

    add csharp 06

---
+++ programovani_v_jave/cviceni_hnetynka/project/src/com/mypackage/task6/Program.java
@@ -0,0 +1,20 @@
+package com.mypackage.task5;
+
+import java.util.Arrays;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+            
+        int[] ar;
+        try{
+            ar = Arrays.stream(args).mapToInt(a -> Integer.parseInt(a)).toArray();
+        } catch (Exception e){
+            System.out.println("Input error");
+            return;
+        }
+        Sorter.QuickSort(ar);
+        Arrays.stream(ar).forEach(a -> System.out.println(a));
+    }
+}
